34ab3999f459ec742cd3e25e23d14e97fba58150
Yinuo Meng <mengyn1999@gmail.com>
2022-12-24 15:23:41 +0000

    Set up the project - Remove unnecessary file - Set up colour variables - Import font - Update favicon - Switch from css to scss - Set up head metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.scss';
+import 'normalize.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { EB_Garamond } from '@next/font/google';
+
+const ebGaramond = EB_Garamond({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main className={ebGaramond.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
