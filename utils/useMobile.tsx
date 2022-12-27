d0441a40caef4d85f44380c64ae3b4ec7d757110
Yinuo Meng <mengyn1999@gmail.com>
2022-12-27 18:34:34 +0000

    Remove debounce from useMobile to prevent flickering

--- utils/useMobile.tsx
+++ utils/useMobile.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import debounce from 'lodash.debounce';
 
 export default function useMobile(width = '860px'): boolean {
   const [isMobile, setIsMobile] = useState(false);
@@ -7,10 +6,9 @@ export default function useMobile(width = '860px'): boolean {
     const mediaQuery = window.matchMedia(`(min-width: ${width})`);
     setIsMobile(!mediaQuery.matches);
     const handler = (e: MediaQueryListEvent): void => setIsMobile(!e.matches);
-    const debouncedHandler = debounce(handler, 250);
-    mediaQuery.addEventListener('change', debouncedHandler);
+    mediaQuery.addEventListener('change', handler);
 
-    () => mediaQuery.removeEventListener('change', debouncedHandler);
+    () => mediaQuery.removeEventListener('change', handler);
   }, [width]);
 
   return isMobile;
